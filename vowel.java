148ac085e8d866742cc2dc69e871caec807519a9
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-07-26 22:03:56 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -14,11 +14,11 @@ class Ideone
         char ch=sc.next().charAt(0);
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
         {
-         System.out.println("Vowel"+ch);
+         System.out.println("Vowel");
         }
         else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
        {
-         System.out.println("Consonant"+ch);
+         System.out.println("Consonant");
  	    }
        else
         System.out.println("Invalid");
