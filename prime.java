3571bafb47384f24e795b3ba8f4eb6e613336b73
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-07-28 10:31:40 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,10 +1,9 @@
-import java.util.*;
+
 class Prime
 {
 public  static void main(String args[])
 {
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
+int n=11;
 int i;
 boolean flag=false;
 for(i=2;i<=n/2;i++)
