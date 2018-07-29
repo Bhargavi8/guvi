31127e1af14821f121b3aa2fb6de66071b67f316
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-07-29 09:50:30 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -5,13 +5,13 @@ public  static void main(String args[])
 {
 int n=11;
 int i;
-boolean flag=false;
+boolean flag=true;
 for(i=2;i<=n/2;i++)
 {
   int temp=n%i;
 if(temp==0)
 {
-flag=true;
+flag=false;
 break;
 }
 }
