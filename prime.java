5da757777c58f2fa83f06d6f64f141a397dfa587
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-07-29 09:42:16 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -8,13 +8,14 @@ int i;
 boolean flag=false;
 for(i=2;i<=n/2;i++)
 {
-if(n%1==0)
+  int temp=n%i;
+if(temp==0)
 {
 flag=true;
 break;
 }
 }
-if(flag==false)
+if(flag)
 System.out.println("yes");
 else
 System.out.println("no");
