61c7967cad40ff55b8567bb536c6c3eedfe7acde
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-04 15:22:50 +0530

    Update max10.java

--- max10.java
+++ max10.java
@@ -7,9 +7,9 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n, max;
+		int n=10, max;
         Scanner s = new Scanner(System.in);
-        n = s.nextInt();
+        
         int a[] = new int[n];
         for(int i = 0; i < n; i++)
         {
