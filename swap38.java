2ca5ee792c30f95b57cfdebc09ab15df2d969d3b
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-07 10:55:54 +0530

    Create swap38.java

---
+++ swap38.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Swap_BitwiseXOR 
+{
+    public static void main(String args[])
+    {
+        int m, n;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        n = s.nextInt();
+        m = m ^ n;
+        n = m ^ n;
+        m = m ^ n;
+        System.out.println(+m);
+        System.out.println(+n);
+    }
+}
