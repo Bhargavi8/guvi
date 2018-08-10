bdc1773f3abe94bd07594abdf2c79a323bedf93c
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-10 12:36:34 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
-class Fibonacci 
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. *
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
 {
-    public static void main(String[] args) 
-    {
-        int n, a = 0, b = 0, c = 1;
-        Scanner s = new Scanner(System.in);
-      
-        n = s.nextInt();
-                for(int i = 1; i <= n; i++)
-        {
-            a = b;
-            b = c;
-            c = a + b;
-            System.out.print(b+" ");
-        }
-    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=0,n2=1,n3,count;
+		count=sc.nextInt();
+		for(int i=0;i<count-1;i++)
+		{
+			n3=n1+n2;
+			System.out.println("  "+n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+		
+
+	}
 }
