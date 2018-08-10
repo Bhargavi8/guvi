7b33381d8e293812d05b42a09848b1602ba77186
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-10 12:22:33 +0530

    Update swap38.java

--- swap38.java
+++ swap38.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
-class Swap_BitwiseXOR 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
 {
-    public static void main(String args[])
-    {
-        int m, n;
-        Scanner s = new Scanner(System.in);
-        m = s.nextInt();
-        n = s.nextInt();
-        m = m ^ n;
-        n = m ^ n;
-        m = m ^ n;
-        System.out.println(+m);
-        System.out.println(+n);
-    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+	}
 }
