bc9745a127ff674943b60ec5ef4df5aa488f31e1
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-10 12:27:28 +0530

    Update max10.java

--- max10.java
+++ max10.java
@@ -7,22 +7,14 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n=10, max;
-        Scanner s = new Scanner(System.in);
-        
-        int a[] = new int[n];
-        for(int i = 0; i < n; i++)
-        {
-            a[i] = s.nextInt();
-        }
-        max = a[0];
-        for(int i = 0; i < n; i++)
-        {
-            if(max < a[i])
-            {
-                max = a[i];
-            }
-        }
-         System.out.println(max);
-	}
+     int n=10;
+     int c;
+     Scanner s=new Scanner(System.in);
+     ArrayList<Integer>a=new ArrayList<Integer>();
+     for(int i=0;i<10;i++){
+        c=s.nextInt();
+        a.add(c);
+     }
+   System.out.print(Collections.max(a));
+    }
 }
