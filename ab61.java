70b8efe089b1887aca6f769cd20357d6fe2ac969
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-13 13:51:52 +0530

    Update and rename 61.java to ab61.java

--- ab61.java
+++ ab61.java
@@ -5,12 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Ideone1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
+      
         String s1=sc.next();
-        System.out.println(s1.substring(0,3));
+          int n=sc.nextInt();
+        String s2=s1.substring(0,n);
+        System.out.println(s2);
 	}
 }
