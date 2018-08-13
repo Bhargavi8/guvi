f2c7be15de852e1c46bcf9fd240a98c08590e4b6
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-13 10:23:56 +0530

    Update and rename search58.java to search158.java

--- search158.java
+++ search158.java
@@ -9,19 +9,19 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-        int i,flag=0;
-		Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-       
-        int a[]=new int[n];
-        for(i=0;i<n;i++)
-        {
-          a[i]=sc.nextInt();
-        }
-         int k=sc.nextInt();
+		int n, x, flag = 0, i = 0;
+        Scanner s = new Scanner(System.in);
+         n = s.nextInt();
+        int a[] = new int[n];
         for(i = 0; i < n; i++)
         {
-            if(a[i] == k)
+            a[i] = s.nextInt();
+        }
+        
+        x = s.nextInt();
+        for(i = 0; i < n; i++)
+        {
+            if(a[i] == x)
             {
                 flag = 1;
                 break;
@@ -31,11 +31,13 @@ class Ideone
                 flag = 0;
             }
         }
-        if(flag==1)
+        if(flag == 1)
         {
-         System.out.println("yes");
+            System.out.println("yes");
         }
         else
-         System.out.println("no");         
+        {
+            System.out.println("no");
+        }
 	}
 }
