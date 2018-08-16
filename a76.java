f381720239f52160c7a2990e0610407578aa95df
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-16 09:47:04 +0530

    Create a76.java

---
+++ a76.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+ class Ideone1{
+      public static void main(String aa[]){
+           int n;
+           int f=0;
+           Scanner s=new Scanner(System.in);
+           n=s.nextInt();
+          for(int i=2;i<n;i++){
+              if(n%i==0){
+                  f=1;
+                  break;
+              }
+          }
+           if(f==1){
+               System.out.println("yes");
+           }
+         else{
+               System.out.println("no");
+         }
+
+    }
+}
