24fc50e15363414a0aefbcff51ba76f64ed07e6d
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-17 12:30:20 +0530

    Update and rename 83.java to a83.java

--- a83.java
+++ a83.java
@@ -1,22 +1,20 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class Ideone1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
         int d=sc.nextInt();
-        int e=a/b;
+        int e = a / b;
         System.out.println(e);
-        int f=c%d;
+        int f = c % d;
         System.out.println(f);
        
 }
