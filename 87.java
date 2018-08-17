e2dc7b37703be50a11de52b99307054d64c1c3fb
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-17 11:01:45 +0530

    Create 87.java

---
+++ 87.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+ static int gcd(int x, int y)
+    {
+        int r=0, a, b;
+        a = (x > y) ? x : y; // a is greater number
+        b = (x < y) ? x : y; // b is smaller number
+ 
+        r = b;
+        while(a % b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return r;
+    }
+ public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+ 
+        System.out.println(gcd(x, y));
+}
+}
