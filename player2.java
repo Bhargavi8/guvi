ac3d416bf74b75fea38cecd34bc02c0a454851bf
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-22 15:56:55 +0530

    Create player2.java

---
+++ player2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+ class Ideone {
+
+     public static void main(String aa[]){
+    int n,fact=1;
+    Scanner s=new Scanner(System.in);
+   n=s.nextInt();
+for(int i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+  System.out.println(fact);
+
+           
+   }
+}
