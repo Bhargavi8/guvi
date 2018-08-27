1367640c994e21bbb65b414b1b68b6db47aa1482
Bhargavi8 <41730516+Bhargavi8@users.noreply.github.com>
2018-08-27 10:32:12 +0530

    Create player1.java

---
+++ player1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+ class Ideone1 {
+
+     public static void main(String aa[]){
+    String a;
+    Scanner s=new Scanner(System.in);
+   a=s.next();
+   String n=String.valueOf(a);
+   String z=new StringBuffer(n).reverse().toString();
+   System.out.println(z);
+
+           
+   }
+}
